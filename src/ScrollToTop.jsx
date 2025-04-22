cc47a5884828c2cb12d78a94597ce871408cf19c
mamunaGulfareen <mamunagulfareen@gmail.com>
2025-04-22 14:30:37 +0500

    add scroll option

--- src/ScrollToTop.jsx
+++ src/ScrollToTop.jsx
@@ -2,12 +2,11 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const ScrollToTop = () => {
-  const location = useLocation();
+  const { pathname } = useLocation();
 
   useEffect(() => {
-    // Reset scroll position to the top on route change
-    window.scrollTo(0, 0);
-  }, [location]);
+    window.scrollTo({ top: 0, left: 0, behavior: "instant" });
+  }, [pathname]);
 
   return null;
 };
